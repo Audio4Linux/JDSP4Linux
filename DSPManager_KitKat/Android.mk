078a78a56567d18d6d2ee6782911ba8a58dde61e
James Fung <james34602@gmail.com>
2015-08-25 09:15:06 +0800

    Disable ODEX to ensure clean apk out

--- DSPManager_KitKat/Android.mk
+++ DSPManager_KitKat/Android.mk
@@ -12,7 +12,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := DSPManager
-
+WITH_DEXPREOPT := false
 include $(BUILD_PACKAGE)
 
 endif
