50557e44f6a651cfbdf8313a921a228f15e0604c
Tim Schneeberger <tim.schneeberger@outlook.de>
2021-12-28 15:29:01 +0100

    Better setup wizard design

--- src/interface/fragment/FirstLaunchWizard.cpp
+++ src/interface/fragment/FirstLaunchWizard.cpp
@@ -26,27 +26,16 @@ FirstLaunchWizard::FirstLaunchWizard(IAudioService *audioService, QWidget *paren
 
 	QTimer::singleShot(500, [&] {
 		ui->p1_icon->startAnimation();
-	});
-	ui->p2_icon->startAnimation();
+    });
 	ui->p3_icon->startAnimation();
 	ui->p4_icon->startAnimation();
 
 	ui->stackedWidget->setAnimation(QEasingCurve::Type::OutCirc);
 	connect(ui->p1_next, &QPushButton::clicked, [&] {
-
-    // always skip
-#if 1 //defined(USE_PULSEAUDIO)
-        // Pulseaudio: skip device selection
-        ui->stackedWidget->slideInIdx(2);
-#else
         ui->stackedWidget->slideInIdx(1);
-#endif
-	});
-	connect(ui->p2_next, &QPushButton::clicked, [&] {
-		ui->stackedWidget->slideInIdx(2);
 	});
 	connect(ui->p3_next, &QPushButton::clicked, [&] {
-		ui->stackedWidget->slideInIdx(3);
+        ui->stackedWidget->slideInIdx(2);
 	});
 	connect(ui->p4_next,     &QPushButton::clicked, [&] {
 		emit wizardFinished();
@@ -55,48 +44,17 @@ FirstLaunchWizard::FirstLaunchWizard(IAudioService *audioService, QWidget *paren
 		QDesktopServices::openUrl(QUrl("https://t.me/joinchat/FTKC2A2bolHkFAyO-fuPjw"));
 	});
 
-	refreshDevices();
-
     ui->p3_systray_disable->setChecked(!AppConfig::instance().get<bool>(AppConfig::TrayIconEnabled));
     ui->p3_systray_enable->setChecked(AppConfig::instance().get<bool>(AppConfig::TrayIconEnabled));
     ui->p3_systray_minOnBoot->setEnabled(AppConfig::instance().get<bool>(AppConfig::TrayIconEnabled));
 
-	QString autostart_path        = AutostartManager::getAutostartPath("jdsp-gui.desktop");
-	bool    autostart_enabled     = AutostartManager::inspectDesktopFile(autostart_path, AutostartManager::Exists);
+    ui->p3_systray_minOnBoot->setChecked(AutostartManager::inspectDesktopFile(AutostartManager::getAutostartPath("jdsp-gui.desktop"),
+                                                                              AutostartManager::Exists));
 
-    ui->p3_systray_minOnBoot->setChecked(autostart_enabled);
+    connect(ui->p3_systray_disable, &QRadioButton::clicked, this, &FirstLaunchWizard::onSystrayRadioSelected);
+    connect(ui->p3_systray_enable,  &QRadioButton::clicked, this, &FirstLaunchWizard::onSystrayRadioSelected);
 
-	auto systray_radio = [this] {
-							 if (lockslot)
-							 {
-								 return;
-							 }
-
-                             AppConfig::instance().set(AppConfig::TrayIconEnabled, ui->p3_systray_enable->isChecked());
-                             ui->p3_systray_minOnBoot->setEnabled(ui->p3_systray_enable->isChecked());
-						 };
-
-	connect(ui->p3_systray_disable, &QRadioButton::clicked, this, systray_radio);
-	connect(ui->p3_systray_enable,  &QRadioButton::clicked, this, systray_radio);
-
-	auto systray_autostart_radio = [this, autostart_path]
-								   {
-									   if (ui->p3_systray_minOnBoot->isChecked())
-									   {
-                                           AutostartManager::saveDesktopFile(autostart_path, AppConfig::instance().get<QString>(AppConfig::ExecutablePath),
-										                                     AutostartManager::inspectDesktopFile(autostart_path, AutostartManager::Delayed));
-									   }
-									   else
-									   {
-										   QFile(autostart_path).remove();
-									   }
-								   };
-
-	connect(ui->p3_systray_minOnBoot,     &QPushButton::clicked,                                                              this, systray_autostart_radio);
-
-    connect(ui->p2_dev_mode_auto,         &QRadioButton::clicked,                                                             this, &FirstLaunchWizard::onDeviceUpdated);
-    connect(ui->p2_dev_mode_manual,       &QRadioButton::clicked,                                                             this, &FirstLaunchWizard::onDeviceUpdated);
-    connect(ui->p2_dev_select,            qOverload<int>(&QComboBox::currentIndexChanged), this, &FirstLaunchWizard::onDeviceUpdated);
+    connect(ui->p3_systray_minOnBoot, &QCheckBox::stateChanged, this, &FirstLaunchWizard::onSystrayAutostartToggled);
 
 }
 
@@ -111,63 +69,28 @@ void FirstLaunchWizard::resizeEvent(QResizeEvent *ev)
     ui->stackedWidget->setMinimumHeight(ev->size().height() * 0.7);
 }
 
-void FirstLaunchWizard::refreshDevices()
-{
-    lockslot = true;
-    ui->p2_dev_select->clear();
-
-    ui->p2_dev_mode_auto->setChecked(AppConfig::instance().get<bool>(AppConfig::AudioOutputUseDefault));
-    ui->p2_dev_mode_manual->setChecked(!AppConfig::instance().get<bool>(AppConfig::AudioOutputUseDefault));
-
-    auto devices = audioService->sinkDevices();
-
-    ui->p2_dev_select->addItem("...", 0);
-    for (const auto& device : devices)
-    {
-        ui->p2_dev_select->addItem(QString("%1 (%2)")
-                                .arg(QString::fromStdString(device.description))
-                                .arg(QString::fromStdString(device.name)), QString::fromStdString(device.name));
-    }
-
-    auto current = AppConfig::instance().get<QString>(AppConfig::AudioOutputDevice);
-
-    bool notFound = true;
-
-    for (int i = 0; i < ui->p2_dev_select->count(); i++)
-    {
-        if (ui->p2_dev_select->itemData(i) == current)
-        {
-            notFound = false;
-            ui->p2_dev_select->setCurrentIndex(i);
-            break;
-        }
-    }
-
-    if (notFound)
-    {
-        QString name = QString("Unknown (%1)").arg(current);
-        ui->p2_dev_select->addItem(name, current);
-        ui->p2_dev_select->setCurrentText(name);
-    }
-    lockslot = false;
-}
-
-void FirstLaunchWizard::onDeviceUpdated()
+void FirstLaunchWizard::onSystrayRadioSelected()
 {
     if (lockslot)
     {
         return;
     }
 
-    AppConfig::instance().set(AppConfig::AudioOutputUseDefault, ui->p2_dev_mode_auto->isChecked());
+    AppConfig::instance().set(AppConfig::TrayIconEnabled, ui->p3_systray_enable->isChecked());
+    ui->p3_systray_minOnBoot->setEnabled(ui->p3_systray_enable->isChecked());
+}
 
-    if (!ui->p2_dev_mode_auto->isChecked())
+void FirstLaunchWizard::onSystrayAutostartToggled(bool isChecked)
+{
+    auto path = AutostartManager::getAutostartPath("jdsp-gui.desktop");
+    if (isChecked)
     {
-        if (ui->p2_dev_select->currentData() == "---")
-        {
-            return;
-        }
-
-        AppConfig::instance().set(AppConfig::AudioOutputDevice, ui->p2_dev_select->currentData());
+        AutostartManager::saveDesktopFile(path,
+                                          AppConfig::instance().get<QString>(AppConfig::ExecutablePath),
+                                          AutostartManager::inspectDesktopFile(path, AutostartManager::Delayed));
+    }
+    else
+    {
+        QFile(path).remove();
     }
 }
--- src/interface/fragment/FirstLaunchWizard.h
+++ src/interface/fragment/FirstLaunchWizard.h
@@ -26,15 +26,14 @@ signals:
 protected:
     void resizeEvent(QResizeEvent* ev) override;
 
+private slots:
+    void onSystrayRadioSelected();
+    void onSystrayAutostartToggled(bool isChecked);
+
 private:
 	Ui::FirstLaunchWizard *ui;
 	bool lockslot = false;
     IAudioService* audioService;
-
-	void refreshDevices();
-
-private slots:
-    void onDeviceUpdated();
 
 };
 
