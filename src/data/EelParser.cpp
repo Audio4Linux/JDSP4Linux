780f085308408c4397d46b40d83a3f649bde71b5
Tim Schneeberger <tim.schneeberger@outlook.de>
2023-01-13 18:31:39 +0100

    liveprog: Fixed regression

--- src/data/EelParser.cpp
+++ src/data/EelParser.cpp
@@ -10,16 +10,60 @@ EELParser::EELParser()
 
 void EELParser::loadFile(QString path)
 {
-	container.path = path;
-	container.code = "";
-	container.reloadCode();
+    container.path = path;
+    container.code = "";
+    container.reloadCode();
 
     clearProperties();
 
-    // Parse number range parameters
+    // Parse parameters
+
+    QRegularExpression descRe(R"((?<var>\w+):(?<def>-?\d+\.?\d*)?<(?<min>-?\d+\.?\d*),(?<max>-?\d+\.?\d*),?(?<step>-?\d+\.?\d*)?>(?<desc>[\s\S][^\n]*))");
+    QRegularExpression descListRe(R"((?<var>\w+):(?<def>-?\d+\.?\d*)?<(?<min>-?\d+\.?\d*),(?<max>-?\d+\.?\d*),?(?<step>-?\d+\.?\d*)?\{(?<opt>[^\}]*)\}>(?<desc>[\s\S][^\n]*))");
+
+    for (const auto &line : container.code.split("\n"))
     {
-        QRegularExpression descRe(R"((?<var>\w+):(?<def>-?\d+\.?\d*)?<(?<min>-?\d+\.?\d*),(?<max>-?\d+\.?\d*),?(?<step>-?\d+\.?\d*)?>(?<desc>[\s\S][^\n]*))");
-        for (const auto &line : container.code.split("\n"))
+        // List parameters
+        {
+            auto matchIterator = descListRe.globalMatch(line);
+
+            if (matchIterator.hasNext())
+            {
+                auto    match = matchIterator.next();
+                QString key   = match.captured("var");
+                QString min   = match.captured("min");
+                QString max   = match.captured("max");
+                QString step  = match.captured("step");
+                QString opt   = match.captured("opt");
+                QString def   = match.captured("def");
+                QString desc  = match.captured("desc").trimmed();
+
+                if (step.isEmpty())
+                {
+                    step = "1";
+                }
+
+                QString current = findVariable(key, EELPropertyType::List);
+
+                if (current == NORESULT)
+                {
+                    break;
+                }
+
+                bool defOk = false;
+                std::optional<float> defaultValue = def.toFloat(&defOk);
+                if(def.isEmpty() || !defOk)
+                    defaultValue = std::nullopt;
+
+                EELListProperty *prop = new EELListProperty(key, desc, defaultValue, current.toInt(),
+                                                            min.toInt(), max.toInt(), opt.split(',', Qt::SkipEmptyParts));
+                properties.append(prop);
+                continue;
+            }
+
+        }
+
+        // Number range parameters
         {
             auto matchIterator = descRe.globalMatch(line);
 
@@ -54,62 +98,22 @@ void EELParser::loadFile(QString path)
                                                                                         min.toFloat(), max.toFloat(),
                                                                                         step.toFloat());
                 properties.append(prop);
+                continue;
             }
         }
     }
 
-    // Parse list parameters
-    {
-        QRegularExpression descRe(R"((?<var>\w+):(?<def>-?\d+\.?\d*)?<(?<min>-?\d+\.?\d*),(?<max>-?\d+\.?\d*),?(?<step>-?\d+\.?\d*)?\{(?<opt>[^\}]*)\}>(?<desc>[\s\S][^\n]*))");
-        for (const auto &line : container.code.split("\n"))
-        {
-            auto matchIterator = descRe.globalMatch(line);
-
-            if (matchIterator.hasNext())
-            {
-                auto    match = matchIterator.next();
-                QString key   = match.captured("var");
-                QString min   = match.captured("min");
-                QString max   = match.captured("max");
-                QString step  = match.captured("step");
-                QString opt   = match.captured("opt");
-                QString def   = match.captured("def");
-                QString desc  = match.captured("desc").trimmed();
-
-                if (step.isEmpty())
-                {
-                    step = "1";
-                }
-
-                QString current = findVariable(key, EELPropertyType::List);
-
-                if (current == NORESULT)
-                {
-                    break;
-                }
-
-                bool defOk = false;
-                std::optional<float> defaultValue = def.toFloat(&defOk);
-                if(def.isEmpty() || !defOk)
-                    defaultValue = std::nullopt;
-
-                EELListProperty *prop = new EELListProperty(key, desc, defaultValue, current.toInt(),
-                                                            min.toInt(), max.toInt(), opt.split(',', Qt::SkipEmptyParts));
-                properties.append(prop);
-            }
-        }
-    }
 }
 
 bool EELParser::saveFile()
 {
-	if (!isFileLoaded())
-	{
-		return false;
-	}
+    if (!isFileLoaded())
+    {
+        return false;
+    }
 
-	container.save();
-	return true;
+    container.save();
+    return true;
 }
 
 bool EELParser::loadDefaults()
@@ -155,10 +159,10 @@ bool EELParser::hasDefaultsDefined()
 
 bool EELParser::canLoadDefaults()
 {
-	if (!isFileLoaded())
-	{
-		return false;
-	}
+    if (!isFileLoaded())
+    {
+        return false;
+    }
 
     for(auto* prop : qAsConst(properties))
     {
@@ -187,30 +191,30 @@ bool EELParser::canLoadDefaults()
 
 bool EELParser::isFileLoaded()
 {
-	return container.codeLoaded;
+    return container.codeLoaded;
 }
 
 QString EELParser::getPath()
 {
-	return container.path;
+    return container.path;
 }
 
 QString EELParser::getDescription()
 {
-	QRegularExpression descRe(R"((?:^|(?<=\n))(?:desc:)([\s\S][^\n]*))");
+    QRegularExpression descRe(R"((?:^|(?<=\n))(?:desc:)([\s\S][^\n]*))");
 
     for (const auto &line : container.code.split("\n"))
     {
         auto matchIterator = descRe.globalMatch(line);
 
         if (matchIterator.hasNext())
-		{
-			auto match = matchIterator.next();
-			return match.captured(1).trimmed();
-		}
-	}
+        {
+            auto match = matchIterator.next();
+            return match.captured(1).trimmed();
+        }
+    }
 
-	return QFileInfo(container.path).fileName();
+    return QFileInfo(container.path).fileName();
 }
 
 EELProperties EELParser::getProperties()
@@ -220,24 +224,24 @@ EELProperties EELParser::getProperties()
 
 bool EELParser::manipulateProperty(EELBaseProperty *propbase)
 {
-	if (propbase->getType() == EELPropertyType::NumberRange)
-	{
-		EELNumberRangeProperty<float> *prop = dynamic_cast<EELNumberRangeProperty<float>*>(propbase);
-		QString                        value;
+    if (propbase->getType() == EELPropertyType::NumberRange)
+    {
+        EELNumberRangeProperty<float> *prop = dynamic_cast<EELNumberRangeProperty<float>*>(propbase);
+        QString                        value;
 
         if (std::floor(prop->getStep()) == prop->getStep()) // is integer?
-		{
-			value = QString::number((int) prop->getValue());
-		}
-		else
-		{
-			value = QString::number(prop->getValue(), 'f', 2);
-		}
+        {
+            value = QString::number((int) prop->getValue());
+        }
+        else
+        {
+            value = QString::number(prop->getValue(), 'f', 2);
+        }
 
-		bool replace_res = replaceVariable(prop->getKey(), value, prop->getType());
-		bool save_res    = saveFile();
-		return replace_res && save_res;
-	}
+        bool replace_res = replaceVariable(prop->getKey(), value, prop->getType());
+        bool save_res    = saveFile();
+        return replace_res && save_res;
+    }
     else if (propbase->getType() == EELPropertyType::List)
     {
         EELListProperty *prop = dynamic_cast<EELListProperty*>(propbase);
@@ -248,7 +252,7 @@ bool EELParser::manipulateProperty(EELBaseProperty *propbase)
         return replace_res && save_res;
     }
 
-	return false;
+    return false;
 }
 
 // --- Private members
@@ -257,23 +261,23 @@ QString EELParser::findVariable(QString         key,
                                 EELPropertyType type)
 {
     if (type == EELPropertyType::NumberRange || type == EELPropertyType::List)
-	{
-		QRegularExpression re(QString(R"(%1\s*=\s*(?<val>-?\d+\.?\d*)\s*;)").arg(key));
+    {
+        QRegularExpression re(QString(R"(%1\s*=\s*(?<val>-?\d+\.?\d*)\s*;)").arg(key));
 
-		for (const auto &line : container.code.split("\n"))
-		{
-			auto matchIterator = re.globalMatch(line);
+        for (const auto &line : container.code.split("\n"))
+        {
+            auto matchIterator = re.globalMatch(line);
 
-			if (matchIterator.hasNext())
-			{
-				auto match = matchIterator.next();
-				return match.captured("val");
-			}
-		}
-	}
+            if (matchIterator.hasNext())
+            {
+                auto match = matchIterator.next();
+                return match.captured("val");
+            }
+        }
+    }
 
     Log::warning(QString("Unable to find a supported variable definition of '%1' in script '%2'").arg(key).arg(getDescription()));
-	return NORESULT;
+    return NORESULT;
 }
 
 bool EELParser::replaceVariable(QString         key,
@@ -281,19 +285,19 @@ bool EELParser::replaceVariable(QString         key,
                                 EELPropertyType type)
 {
     if (type == EELPropertyType::NumberRange || type == EELPropertyType::List)
-	{
-		QRegularExpression re(QString(R"(%1\s*=\s*(?<val>-?\d+\.?\d*)\s*;)").arg(key));
-		auto               matchIterator = re.globalMatch(container.code);
+    {
+        QRegularExpression re(QString(R"(%1\s*=\s*(?<val>-?\d+\.?\d*)\s*;)").arg(key));
+        auto               matchIterator = re.globalMatch(container.code);
 
-		if (matchIterator.hasNext())
-		{
-			auto match = matchIterator.next();
-			int  start = match.capturedStart("val");
-			int  len   = match.capturedLength("val");
-			container.code.remove(start, len);
-			container.code.insert(start, value);
-			return true;
-		}
+        if (matchIterator.hasNext())
+        {
+            auto match = matchIterator.next();
+            int  start = match.capturedStart("val");
+            int  len   = match.capturedLength("val");
+            container.code.remove(start, len);
+            container.code.insert(start, value);
+            return true;
+        }
     }
 
     return false;
--- src/data/EelParser.h
+++ src/data/EelParser.h
@@ -133,6 +133,7 @@ public:
 	{
 		type = EELPropertyType::NumberRange;
 	};
+
 	~EELNumberRangeProperty() {}
 
 	TNum getValue() const
