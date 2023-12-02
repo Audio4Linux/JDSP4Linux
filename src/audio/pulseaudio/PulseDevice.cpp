2c99e52c650dd46873867d8966684d998ee63757
Tim Schneeberger <tim.schneeberger@outlook.de>
2023-12-02 23:25:37 +0100

    fix: fixed pulseaudio regression

--- src/audio/pulseaudio/PulseDevice.cpp
+++ src/audio/pulseaudio/PulseDevice.cpp
@@ -5,5 +5,6 @@ PulseDevice::PulseDevice(const mySinkInfo &info)
     id = info.index;
     name = info.name;
     description = info.description;
-    output_route = info.active_port;
+    output_route_name = info.active_port;
+    output_route_description = info.active_port;
 }
